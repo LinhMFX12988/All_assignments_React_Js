0004659b070d53f3294e6a31d13bd168a6668a47
React <linhnmfx12988@funix.edu.vn>
2021-12-27 09:45:16 +0700

    hook

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -4,44 +4,31 @@ import dateFormat from "dateformat";
 
 function Menu(props) {
 
-  const [dishSelect, setDishSelect] = useState(null)
+  const [dishSelect, setDishSelect] = useState('Bấm vào tên nhân viên để xem thông tin')
 
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     selectedDish: null,
-  //   };
-  // }
-
-  // onDishSelect(staffs) {
-  //   this.setState({ selectedDish: staffs });
-  // }
-
-  // renderDish(staffs) {
-  //   if (staffs != null)
-  //     return (
-  //       <CardBody>
-  //         <h3>{"Họ và tên: " + staffs.name}</h3>
-  //         <CardText>
-  //           {"Ngày sinh: " + dateFormat(staffs.doB, "dd/mm/yyyy")}
-  //         </CardText>
-  //         <CardText>
-  //           {"Ngày vào công ty: " + dateFormat(staffs.startDate, "dd/mm/yyyy")}
-  //         </CardText>
-  //         <CardText>{"Phòng ban: " + staffs.department.name}</CardText>
-  //         <CardText>{"Số ngày nghỉ còn lại: " + staffs.annualLeave}</CardText>
-  //         <CardText>{"Số ngày đã làm thêm: " + staffs.overTime}</CardText>
-  //       </CardBody>
-  //     );
-  //   else return <div>Bấm vào tên nhân viên để xem thông tin</div>;
-  // }
-
+  const renderDish = (staffs) => {
+      return (
+        <CardBody>
+          <h3>{"Họ và tên: " + staffs.name}</h3>
+          <CardText>
+            {"Ngày sinh: " + dateFormat(staffs.doB, "dd/mm/yyyy")}
+          </CardText>
+          <CardText>
+            {"Ngày vào công ty: " + dateFormat(staffs.startDate, "dd/mm/yyyy")}
+          </CardText>
+          <CardText>{"Phòng ban: " + staffs.department.name}</CardText>
+          <CardText>{"Số ngày nghỉ còn lại: " + staffs.annualLeave}</CardText>
+          <CardText>{"Số ngày đã làm thêm: " + staffs.overTime}</CardText>
+        </CardBody>
+      );
+  }
+  
     const menu = props.dishes.map((staffs) => {
       return (
         <div key={staffs.id} className="col- col-md-6 col-xl-4">
-          {/* <Card onClick={() => this.dishSelect(staffs)}> */}
+          <Card onClick={setDishSelect}>
             <CardTitle>{staffs.name}</CardTitle>
-          {/* </Card> */}
+          </Card>
         </div>
       );
     });
@@ -51,7 +38,7 @@ function Menu(props) {
         <div className="row">{menu}</div>
         <div className="row">
           <div className="col-12 col-md-12 col-xl-12">
-            {/* {this.renderDish(this.state.selectedDish)} */}
+            {dishSelect}
           </div>
         </div>
       </div>
