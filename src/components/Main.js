4c042eb1483db9715c4ca0b7bc7b4d0b9fd5efb2
React <linhnmfx12988@funix.edu.vn>
2021-12-28 22:29:41 +0700

    zzzzzzzzzzzz

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,34 @@
 import React, { Component } from "react";
 import Header from "./Header";
-import Footer from "./Footer";
 import LishStaffs from "./ListStaffs";
-import { STAFFS } from "../shared/staffs";
+import StaffDetail from "./DetailStaff";
+import Footer from "./Footer";
+import { STAFFS, DEPARTMENTS } from "../shared/staffs";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dishes: STAFFS,
+      departments: DEPARTMENTS,
     };
   }
   render() {
+
+    const StaffWithId = ({ match }) => 
+      <StaffDetail
+        staffs={this.state.staffs.filter((staff) => staff.id === parseInt(match.params.id, 10))[0]}
+      />
+
     return (
       <div className="container">
         <Header />
-        <LishStaffs dishes={this.state.dishes} />
+        <Switch>
+          <Route exact path="/staffs" component={() => <LishStaffs dishes={this.state.dishes} />} />
+          <Route path="/staffs/:id" component={StaffWithId} />
+          {/* <Redirect to="/staffs" /> */}
+        </Switch>
         <Footer />
       </div>
     );
